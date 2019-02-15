ce07962b1d7618b568c263e7beac7dff07b57ed4
maxramirez84 <maxramirez84@gmail.com>
2019-02-15 11:00:14 +0100

    Applied Google's R style Guide (Identifiers)

--- approached_households_map.R
+++ approached_households_map.R
@@ -4,72 +4,72 @@ library(purrr)
 library(raster)
 
 # File names
-data_file_name <- "DATA/TIPTOPHHSBaselineDRC_DATA_WITH_NO_DUPS_2018-06-19_1600.csv"
-cluster_metadata_file_name <- "DATA/TIPTOPHHSBaselineDRC_CLUSTERS_BULUNGU.csv"
+kDataFileName <- "DATA/TIPTOPHHSBaselineDRC_DATA_WITH_NO_DUPS_2018-06-19_1600.csv"
+kClusterMetadataFileName <- "DATA/TIPTOPHHSBaselineDRC_CLUSTERS_BULUNGU.csv"
 
 # Parameters
-district_code <- 2
-district_name <- "bulungu"
+kDistrictCode <- 2
+kDistrictName <- "bulungu"
 
 # Column/Variable names
-district_cluster_column <- paste0("cluster_", district_name)
-district_cluster_name_column <- paste0(district_cluster_column, "_name")
+kDistrictClusterColumn <- paste0("cluster_", kDistrictName)
+kDistrictDlusterNameColumn <- paste0(kDistrictClusterColumn, "_name")
 
 # Labels
-household_label <- "%s | Household %s"
+kHouseholdLabel <- "%s | Household %s"
 
 # Read and pre-format GPS disctrict data: Divide data by clusters
-hhs_data <- read.csv(data_file_name)
-district_data <- hhs_data[hhs_data$district == district_code, ]
-district_data.cluster <- split(district_data, district_data[district_cluster_column])
+hhs.data <- read.csv(kDataFileName)
+district.data <- hhs.data[hhs.data$district == kDistrictCode, ]
+district.data.cluster <- split(district.data, district.data[kDistrictClusterColumn])
 
 # Read clusters' metadata: names
-district_clusters <- read.csv(cluster_metadata_file_name)
+district.clusters <- read.csv(kClusterMetadataFileName)
 
 # Create Leaflet map widget from a tile layer (OpenStreetMap)
-l <- leaflet() %>% addProviderTiles(providers$OpenStreetMap.Mapnik)
+map <- leaflet() %>% addProviderTiles(providers$OpenStreetMap.Mapnik)
 
 # Put households and clusters' boundaries on the map
-names(district_data.cluster) %>%
+names(district.data.cluster) %>%
   # For each cluster (walk), geopositionate households and boundaries 
   walk(function(cluster) {
     #browser()
     # Cluster boundaries defined as a circle in which the center is the centroid of GPS points 
     # forming the cluster and the radius is the distance between this center and the farthest point
-    centroid_lng <- mean(district_data.cluster[[cluster]]$longitude, na.rm = T)
-    centroid_lat <- mean(district_data.cluster[[cluster]]$latitude, na.rm = T)
+    centroid.lng <- mean(district.data.cluster[[cluster]]$longitude, na.rm = T)
+    centroid.lat <- mean(district.data.cluster[[cluster]]$latitude, na.rm = T)
     radius <- max(pointDistance(
-      p1 = c(centroid_lng, centroid_lat), 
-      p2 = district_data.cluster[[cluster]][c("longitude", "latitude")],
+      p1 = c(centroid.lng, centroid.lat), 
+      p2 = district.data.cluster[[cluster]][c("longitude", "latitude")],
       lonlat = T
     ), na.rm = T)
     
     # Look up cluster name
-    cluster_name <- district_clusters[district_clusters[district_cluster_column] == cluster,
-                                      district_cluster_name_column]
+    cluster.name <- district.clusters[district.clusters[kDistrictClusterColumn] == cluster,
+                                      kDistrictDlusterNameColumn]
     
-    l <<- l %>%
+    map <<- map %>%
       # Add a marker for each geopositionated household in the cluster
       addMarkers(
-        data = district_data.cluster[[cluster]],
+        data = district.data.cluster[[cluster]],
         lng = ~longitude,
         lat = ~latitude,
-        label = ~sprintf(household_label, cluster_name, household),
-        group = cluster_name,
+        label = ~sprintf(kHouseholdLabel, cluster.name, household),
+        group = cluster.name,
         clusterOptions = markerClusterOptions(removeOutsideVisibleBounds = F)
       ) %>%
       # Add a circle for each cluster (proxy of cluster boundaries)
       addCircles(
-        lng = centroid_lng,
-        lat = centroid_lat,
-        label = cluster_name,
+        lng = centroid.lng,
+        lat = centroid.lat,
+        label = cluster.name,
         weight = 1,
         radius = radius,
-        group = cluster_name
+        group = cluster.name
       )
   })
 
 # Put layers control to display or hide concrete clusters
-l <- l %>% addLayersControl(overlayGroups = district_clusters[[district_cluster_name_column]])
+map <- map %>% addLayersControl(overlayGroups = district.clusters[[kDistrictDlusterNameColumn]])
 
-l
+map
